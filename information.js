f7fbb1b5262589b1907e150bc5854f46bdfe1a04
BrijeshSavjani <21848@mynsb.co.uk>
2021-02-01 03:41:42 +0000

    Genralise Search Codes
    For Objects w/ more or less attributes

--- information.js
+++ information.js
@@ -1,8 +1,8 @@
 
      var data = [
-        {"Name": "Name1", "Attribute1": "Attribute1_Value", "Attribute2": "Attribute2_Value1","Attribute3": true },
-        {"Name": "Name2", "Attribute1": "Attribute1_Value", "Attribute2": "Attribute2_Value2","Attribute3": false},
-        {"Name": "Name3", "Attribute1": "Attribute1_Value", "Attribute2": "Attribute2_Value3","Attribute3": true},
-        {"Name": "Name4", "Attribute1": "Attribute1_Value", "Attribute2": "Attribute2_Value4","Attribute3": false},
-        {"Name": "Name5", "Attribute1": "Attribute1_Value", "Attribute2": "Attribute2_Value5","Attribute3": true}
+        {"Name": "Name1", "Attribute2": "Attribute2_Value1","Attribute3": true },
+        {"Name": "Name2", "Attribute2": "Attribute2_Value2","Attribute3": false},
+        {"Name": "Name3", "Attribute2": "Attribute2_Value3","Attribute3": true},
+        {"Name": "Name4", "Attribute2": "Attribute2_Value4","Attribute3": false},
+        {"Name": "Name5", "Attribute2": "Attribute2_Value5","Attribute3": true}
     ];
